739f4c0cc7049d79861722d1078c95d4aa8f3155
chinmay5525 <chinmayawade32@yahoo.com>
2025-01-02 11:54:54 +0530

    asa

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -94,21 +94,6 @@ const Home = () => {
         }
     ];
 
-    const scrollContainerRef = useRef(null);
-
-    const scrollLeft = () => {
-        scrollContainerRef.current.scrollBy({
-            left: -300, // Scroll left by 300px
-            behavior: 'smooth',
-        });
-    };
-
-    const scrollRight = () => {
-        scrollContainerRef.current.scrollBy({
-            left: 300, // Scroll right by 300px
-            behavior: 'smooth',
-        });
-    };
     return (
         <div>
             <section>
